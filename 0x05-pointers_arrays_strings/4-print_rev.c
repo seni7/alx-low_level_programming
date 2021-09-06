3a522aee496813b2c9e93f0d59beb50718a6cb7f
seni7 <85029836+seni7@users.noreply.github.com>
2021-09-06 22:28:09 +0300

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse with new line
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+		;
+	--i;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		--i;
+	}
+	_putchar('\n');
+}
