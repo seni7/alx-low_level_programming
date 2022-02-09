b60d817fac2b38d943199cde478ac3342f742067
seni7 <85029836+seni7@users.noreply.github.com>
2022-02-09 10:54:17 +0300

    pointer
    ....

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,33 +1,37 @@
-#include "function_pointers.h"
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - Prints the opcodes of main function
- * @argc: Number of command line arguments
- * @argv: Array containing command line arguments
- *
- * Return: 0
- */
-int main(int argc, char **argv)
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
 {
-	int byte, i;
+	int i, n;
+
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	byte = atoi(argv[1]);
-	if (byte < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < byte; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%02hhx", ((char *)main)[i]);
-		if (i < byte - 1)
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
 			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
